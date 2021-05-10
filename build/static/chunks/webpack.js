aeefe0d1822daa695e9d62c2cfaae200fe590090
PDude <dudich.pasha@gmail.com>
2021-05-10 12:45:15 +0300

    fixed fonts

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ebf2a446014e5e7fbdc0";
+/******/ 	var hotCurrentHash = "ac3ac4551efa02d3e51f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,12 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head />
         <title>Siteen Agency</title>
-        <link rel='icon' href='/favicon.ico'></link>
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap'
+          rel='stylesheet'
+        />
         <meta name='description' content='Free Web tutorials' />
         <meta name='keywords' content='HTML, CSS, JavaScript' />
         <meta name='author' content='John Doe' />
