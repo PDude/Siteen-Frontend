ff11faa3efc06d6e920f16b88814957bfb7e4f59
PDude <dudich.pasha@gmail.com>
2021-05-12 13:01:21 +0300

    changed FUCKING name FOOTER

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "589dee7a0b306e36a609";
+/******/ 	var hotCurrentHash = "2b11906e1c52d0182591";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
