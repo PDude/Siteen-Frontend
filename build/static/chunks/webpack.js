4962cb6696f6d4334ca41d16b76a755253892c54
PDude <dudich.pasha@gmail.com>
2021-05-16 22:13:14 +0300

    some sasha changes

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ad26875c23cbff5ef935";
+/******/ 	var hotCurrentHash = "9e44aed09cbd67dcc515";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
